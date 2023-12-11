acfa4a5d9fd7ba0044f98ac3dc4dfb1461cf3048
SBNTT <bysebastien@gmail.com>
2023-12-11 20:09:49 +0100

    2023-8-p2

--- src/main/kotlin/2023_8.kt
+++ src/main/kotlin/2023_8.kt
@@ -23,7 +23,34 @@ private fun part1() = println("Run duration: ${measureTimeMillis {
 }} ms")
 
 private fun part2() = println("Run duration: ${measureTimeMillis {
-  
+    val lines = getResourceAsLinesStream("2023_8.txt").toList()
+
+    val instructions = lines.first().toCharArray().map {
+        if (it == 'L') 0 else 1
+    }
+    
+    val map = lines.subList(2, lines.size).associate { line ->
+        val parts = line.split(" = (", ", ", ")")
+        Pair(parts[0], listOf(parts[1], parts[2]))
+    }
+
+    var acc = 0
+    val states = map.keys.filter { it.last() == 'A' }.toMutableList()
+    var state: String
+    
+    val steps = states.map { 
+        acc = 0
+        state = it
+
+        while (state.last() != 'Z') {
+            state = map[state]!![instructions[acc % instructions.size]]
+            acc += 1
+        }
+        
+        acc
+    }
+    
+    println(steps.lcm())
 }} ms")
 
 fun main() {
@@ -31,5 +58,5 @@ fun main() {
     part1() // 11567
     println("=====")
     println("part 2:")
-    part2()
+    part2() // 9858474970153
 }
--- src/main/kotlin/utils.kt
+++ src/main/kotlin/utils.kt
@@ -5,3 +5,25 @@ fun getResourceAsLinesStream(name: String) =
         .bufferedReader()
         .lines()
         .asSequence()
+
+
+fun lcm(a: Long, b: Long): Long {
+    val larger = if (a > b) a else b
+    val maxLcm = a * b
+    var lcm = larger
+    while (lcm <= maxLcm) {
+        if (lcm % a == 0L && lcm % b == 0L) {
+            return lcm
+        }
+        lcm += larger
+    }
+    return maxLcm
+}
+
+fun List<Int>.lcm(): Long {
+    var result = get(0).toLong()
+    for (i in 1..<size) {
+        result = lcm(result, get(i).toLong())
+    }
+    return result
+}
